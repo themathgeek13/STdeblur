e424e63f2f718aa76f93a903b54a04a226abeaf7
Rohan Rao <themathgeek13@gmail.com>
2018-06-30 19:15:54 +0530

    added GPU support

--- DeblurGAN/models/losses.py
+++ DeblurGAN/models/losses.py
@@ -24,9 +24,9 @@ class PerceptualLoss():
 	def contentFunc(self):
 		conv_3_3_layer = 14
 		cnn = models.vgg19(pretrained=True).features
-		#cnn = cnn.cuda()
+		cnn = cnn.cuda()
 		model = nn.Sequential()
-		# model = model.cuda()
+		model = model.cuda()
 		for i,layer in enumerate(list(cnn)):
 			model.add_module(str(i),layer)
 			if i == conv_3_3_layer:
@@ -138,17 +138,17 @@ class DiscLossWGANGP(DiscLossLS):
 	def calc_gradient_penalty(self, netD, real_data, fake_data):
 		alpha = torch.rand(1, 1)
 		alpha = alpha.expand(real_data.size())
-		# alpha = alpha.cuda()
+		alpha = alpha.cuda()
 
 		interpolates = alpha * real_data + ((1 - alpha) * fake_data)
 
-		# interpolates = interpolates.cuda()
+		interpolates = interpolates.cuda()
 		interpolates = Variable(interpolates, requires_grad=True)
 		
 		disc_interpolates = netD.forward(interpolates)
 
 		gradients = autograd.grad(outputs=disc_interpolates, inputs=interpolates,
-								  grad_outputs=torch.ones(disc_interpolates.size()),#.cuda(),
+								  grad_outputs=torch.ones(disc_interpolates.size()).cuda(),
 								  create_graph=True, retain_graph=True, only_inputs=True)[0]
 
 		gradient_penalty = ((gradients.norm(2, dim=1) - 1) ** 2).mean() * self.LAMBDA
